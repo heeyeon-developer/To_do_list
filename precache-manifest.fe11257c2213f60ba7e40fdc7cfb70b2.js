00aaac7ee7b87f8abf6035e2b3eea6855efe3060
heeyeon_kim <54020487+heeyeon-developer@users.noreply.github.com>
2020-10-22 23:45:08 +0900

    Updates

---
+++ precache-manifest.fe11257c2213f60ba7e40fdc7cfb70b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038714efd0efe7a8e83c483cc0647af2",
+    "url": "/To_do_list/index.html"
+  },
+  {
+    "revision": "2820c229f0467b71529c",
+    "url": "/To_do_list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb0e5b2d328683a1ab05",
+    "url": "/To_do_list/static/js/2.05d193f2.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/To_do_list/static/js/2.05d193f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2820c229f0467b71529c",
+    "url": "/To_do_list/static/js/main.2c069884.chunk.js"
+  },
+  {
+    "revision": "e4844b890429fdf2333d",
+    "url": "/To_do_list/static/js/runtime-main.bb364de2.js"
+  }
+]);
